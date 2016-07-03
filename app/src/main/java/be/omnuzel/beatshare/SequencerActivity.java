e673a2140f7a655545e8105859cdd7cb4a9d18aa
Oyzuu <oyasumiiiii@gmail.com>
2016-07-03 22:14:57 +0200

    SoundBank & refactor - AND FIX

--- app/src/main/java/be/omnuzel/beatshare/SequencerActivity.java
+++ app/src/main/java/be/omnuzel/beatshare/SequencerActivity.java
@@ -8,6 +8,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import be.omnuzel.beatshare.classes.SoundBank;
@@ -17,7 +18,7 @@ public class SequencerActivity extends AppCompatActivity {
     // TODO Check AudioAttributes CONTENT_TYPE and USAGE
     // TODO Modify onLoadComplete() for Button / sample id
 
-    private SoundBank soundBank;
+    private SoundBank          soundBank;
     private LinkedList<Button> buttons = new LinkedList<>();
 
     @Override
@@ -25,7 +26,11 @@ public class SequencerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sequencer);
 
-        for (int i = 0; i < 16; i++) {
+        // For matching coherence on button / sample id - NO ZERO BUTTON !
+        buttons.add(null);
+
+        // Init all the buttons with a setActionTouch and store them in a list for onLoadComplete
+        for (int i = 1; i <= 16; i++) {
             String buttonId = "seq_button_" + i;
             int    id       = getResources().getIdentifier(buttonId, "id", getPackageName());
             Button button   = (Button) findViewById(id);
@@ -43,12 +48,22 @@ public class SequencerActivity extends AppCompatActivity {
     public void initSounds() {
         soundBank = new SoundBank(this);
 
-        soundBank.load(R.raw.iamm_c1_bass_drum, R.id.seq_button_0);
+        soundBank.load(R.raw.iamm_c1_bass_drum,      R.id.seq_button_1);
+        soundBank.load(R.raw.iamm_d1_acoustic_snare, R.id.seq_button_2);
+        soundBank.load(R.raw.iamm_c3_hi_bongo,       R.id.seq_button_3);
+        soundBank.load(R.raw.iamm_cd3_low_bongo,     R.id.seq_button_4);
+        soundBank.load(R.raw.iamm_fd1_closed_hihat,  R.id.seq_button_5);
+        soundBank.load(R.raw.iamm_ad1_open_hihat,    R.id.seq_button_6);
+        soundBank.load(R.raw.iamm_e1_electric_snare, R.id.seq_button_7);
+
+        // First sound seems to load instantly and thus is not "heard" on the listener - DUCT TAPE
+        buttons.get(1).setBackground(getResources().getDrawable(R.drawable.sequencer_pad));
 
         soundBank.getSoundPool().setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
             public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                buttons.get(sampleId - 1)
+                Log.i("SEQ_LOADCOMPLETE", "Sample " + sampleId + " loaded !");
+                buttons.get(sampleId)
                         .setBackground(getResources().getDrawable(R.drawable.sequencer_pad));
             }
         });
--- app/src/main/java/be/omnuzel/beatshare/classes/SoundBank.java
+++ app/src/main/java/be/omnuzel/beatshare/classes/SoundBank.java
@@ -5,17 +5,27 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
+import android.util.Log;
 
 import java.util.HashMap;
 
 public class SoundBank {
+
+    public static SoundBank instance;
+    public static SoundBank getInstance(Context context) {
+        if (instance == null)
+            instance = new SoundBank(context);
+        return instance;
+    }
+
     private SoundPool soundPool;
-    private Context context;
+    private Context   context;
+
     private HashMap<Integer, Integer> pads = new HashMap<>();
 
     public SoundBank(Context context) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            soundPool    = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
+            soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
         }
         else {
             AudioAttributes audioAttributes = new AudioAttributes.Builder()
@@ -34,11 +44,13 @@ public class SoundBank {
 
     public void load(int resourceId, int buttonId) {
         int id = soundPool.load(context, resourceId, 0);
+        Log.i("SOUNDBANK_LOAD", "Sample " + id + " loaded !");
         pads.put(buttonId, id);
     }
 
     public void play(int buttonId) {
-        soundPool.play(pads.get(buttonId), 1, 1, 1, 0, 1);
+        if (pads.get(buttonId) != null)
+            soundPool.play(pads.get(buttonId), 1, 1, 1, 0, 1);
     }
 
     public SoundPool getSoundPool() {
